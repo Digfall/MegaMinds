ef28be6c39f3698078d54c6323e33ad0c87e7515
Digfall <111002239+Digfall@users.noreply.github.com>
2024-04-13 19:21:59 +0500

    Создал спавн мобов через картинку

--- game/Assets/Scripts/SpawnPlayer.cs
+++ game/Assets/Scripts/SpawnPlayer.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // Добавьте эту директиву, если она ещё не добавлена
 
 public class SpawnPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject playerPrefab; // префаб объекта Player
+
+    public Transform spawnPosition; // позиция, где будет создаваться Player
+
+    public void SpawnPlayers()
     {
+        // Создаем объект Player на указанной позиции
+        GameObject playerInstance = Instantiate(playerPrefab, spawnPosition.position, Quaternion.identity);
 
-    }
+        // Получаем компонент Player из созданного объекта Player
+        Player playerComponent = playerInstance.GetComponent<Player>();
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Если компонент Player найден
+        if (playerComponent != null)
+        {
+            // Получаем компонент Healthbar из объекта Player
+            HealthBar healthBar = playerComponent.GetComponentInChildren<HealthBar>();
 
+            // Если компонент Healthbar найден
+            if (healthBar != null)
+            {
+                // Активируем объект Healthbar (Slider)
+                healthBar.gameObject.SetActive(true);
+            }
+            else
+            {
+                Debug.LogError("Healthbar component not found on the playerPrefab.");
+            }
+        }
+        else
+        {
+            Debug.LogError("Player component not found on the playerPrefab.");
+        }
     }
 }
---
+++ game/Assets/Scripts/panelactive.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panelactive : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject panelll;
+    void Start()
+    {
+        panelll.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
